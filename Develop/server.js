63d1050536b4702b65c32c6c8a0fe66d8579abdf
Brandon Diaz <brandonjustindiaz@yahoo.com>
2021-12-17 18:13:28 -0500

    added function to read db and finished get api notes function to read database and return json

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ const dbJson= ('db/db.json');
 // this looks at the folder called public and loads all of the files 
 // without having to write the full path for each item in that folder to load
 app.use(express.static('public'));
-
+app.use(express.json());
 // below is basically home address
 // GET * should return the index.html file:
 
@@ -28,13 +28,30 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
 // GET /api/notes should read the db.json file and return all saved notes as JSON:
+// return all saved notes as json saved notes are going to be held in the db.json
+app.get('/api/notes', (req, res) =>{
+let data= readDataBase();
+let notes = res.json(data)
+return notes
+});
+
+// function to read db.json 
+function readDataBase(){
+let data= fs.readFileSync(path.join(__dirname, '/db/db.json'));
+console.log(JSON.parse(data));
+return JSON.parse(data);
+};
 
 // POST /api/notes should receive a new note to save on the request body, 
 // add it to the db.json file, 
 // and then return the new note to the client. 
 // You'll need to find a way to give each note a unique id when it's saved
 
+// app.post('/api/notes', (req, res) =>
+
+// );
 
 // Bonus:
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
