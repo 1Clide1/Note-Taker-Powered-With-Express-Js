777970c116a096e0430339fb4cba8ca98f5a6a21
Brandon Diaz <brandonjustindiaz@yahoo.com>
2021-12-17 17:05:11 -0500

    added more of the base and added the get home function

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,19 @@
 // import express
-const express = require('express')
-const app= express()
-// require everything else that I need for this project
-const fs= require('fs')
+const express = require('express');
+const app= express();
+// require/get everything else that I need for this project
+const PORT = 3001;
+// fs module for db.json file
+const fs= require('fs');
+const path= require('path');
+const dbJson= ('db/db.json');
+// get express middlewear/express functions if needed
+app.use(express.static('public'));
 // below is basically home address
 // GET * should return the index.html file.
-
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 // below is website/notes
 // GET /notes should return the notes.html file.
 
@@ -21,3 +29,8 @@ const fs= require('fs')
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
 // In order to delete a note, you'll need to read all notes from the db.json file, 
 // remove the note with the given id property, and then rewrite the notes to the db.json file.
+
+// listen to the port that way I can personally test if the backend is working
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
