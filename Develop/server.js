852a120b782ee9f675091d50bf4f392ad57bad34
Brandon Diaz <brandonjustindiaz@yahoo.com>
2021-12-17 23:38:02 -0500

    added proper save feature to app.post /api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -9,11 +9,14 @@ const PORT = 3001;
 const fs= require('fs');
 const path= require('path');
 const dbJson= ('db/db.json');
+// using helper module from previous class to help me with id numbers
+const uuid = require('./helpers/uuid');
 // get express middlewear/express functions if needed:
 
 // this looks at the folder called public and loads all of the files 
 // without having to write the full path for each item in that folder to load
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // below is basically home address
 // GET * should return the index.html file:
@@ -37,27 +40,58 @@ let notes = res.json(data)
 return notes
 });
 
-// function to read db.json 
-function readDataBase(){
-let data= fs.readFileSync(path.join(__dirname, '/db/db.json'));
-console.log(JSON.parse(data));
-return JSON.parse(data);
-};
 
+    
 // POST /api/notes should receive a new note to save on the request body, 
 // add it to the db.json file, 
 // and then return the new note to the client. 
 // You'll need to find a way to give each note a unique id when it's saved
 
-// app.post('/api/notes', (req, res) =>
-
-// );
+app.post('/api/notes', (req, res) =>{
+const { title, text }= req.body
+if (title && text) {
+    const newNotes= {
+        title,
+        text,
+        note_id: uuid()
+    }
+// read the db
+    let data= readDataBase()
+// add new content to file
+    data= {...data, [newNotes.note_id]: newNotes}
+    // stringify the data
+    const notesStringified= JSON.stringify(data, null, 2)
+    console.log(data)
+    saveToDataBase(dbJson, notesStringified)
+}
+else{
+    res.json('error in posting notes')
+}
+});
 
 // Bonus:
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
 // In order to delete a note, you'll need to read all notes from the db.json file, 
 // remove the note with the given id property, and then rewrite the notes to the db.json file.
+// app.delete('/api/notes/:id', (req, res) =>
 
+// );
+// function to read db.json 
+function readDataBase(){
+    let data= fs.readFileSync(path.join(__dirname, dbJson));
+    // console.log(JSON.parse(data));
+    return JSON.parse(data);
+};
+// function to save
+function saveToDataBase(db, data){
+    fs.writeFile(db, data, (err) =>
+          err
+            ? console.error(err)
+            : console.log(
+                `Notes have been saved to the db`
+              )
+              );
+};
 // listen to the port that way I can personally test if the backend is working
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
